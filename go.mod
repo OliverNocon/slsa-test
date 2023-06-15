2ce6832d5911dcca5efcb48a577b6f4d77f94921
Oliver Nocon <oliver.nocon@sap.com>
2023-06-15 13:24:44 +0200

    chore: add slsa go builder

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/OliverNocon/slsa-test
 
 go 1.20
+
+require github.com/secure-systems-lab/go-securesystemslib v0.6.0
+
+require (
+	golang.org/x/crypto v0.9.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,9 +1,39 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/secure-systems-lab/go-securesystemslib/dsse"
+	// intoto "github.com/in-toto/in-toto-golang/in_toto"
 )
 
 func main() {
 	fmt.Println("hello SLSA")
+	filename := "multiple.intoto.jsonl"
+	f, err := os.ReadFile(filename)
+	if err != nil {
+		log.Fatal("failed to read file", err)
+	}
+
+	var envelope dsse.Envelope
+
+	if err := json.Unmarshal(f, &envelope); err != nil {
+		log.Fatal("failed to unmarshal ", filename, ": ", err)
+	}
+
+	contentBytes, err := envelope.DecodeB64Payload()
+	if err != nil {
+		log.Fatal("failed to decode payload", err)
+	}
+
+	var payload map[string]any
+	if err := json.Unmarshal(contentBytes, &payload); err != nil {
+		log.Fatal("failed to decode payload", err)
+	}
+
+	out, _ := json.MarshalIndent(payload, "", "    ")
+	fmt.Print(string(out))
 }
